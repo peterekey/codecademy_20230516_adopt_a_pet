811efe4aa8508dbc6fdd2f8b43a466dbb96e4a52
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-17 08:49:19 +1000

    Step 5: Initialise the appRouter variable with a new Route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = <Route path="/" element={<Root />}/>
 
 function App() {
   return (
