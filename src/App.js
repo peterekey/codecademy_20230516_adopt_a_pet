e263adb8de1d81fea89f0cd0bbf0d8de7c927892
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-19 07:07:55 +1000

    Step 12: Add nested route under Root route to render PetDetailsPage component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route path="/" element={<HomePage />} />
     <Route path=":type" element={<HomePage />} />
+    <Route path=":type/:id" element={<PetDetailsPage />} />
   </Route>
 ))
 
